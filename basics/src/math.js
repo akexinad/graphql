1348599d41a3acb3ea621e973a1de214c474da42
danny <danixeka@gmail.com>
2019-05-12 23:37:04 +1000

    ignoring workshop

---
+++ basics/src/math.js
@@ -0,0 +1,11 @@
+export function add(a, b) {
+    const c = a + b
+    return console.log(c);
+}
+
+function subtract(a, b) {
+    const c = a - b;
+    return console.log(c);
+}
+
+export subtract;
