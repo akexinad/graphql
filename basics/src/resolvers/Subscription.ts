9ff14d788151b985ff713bfc82bacacaa48fd097
akexinad <danixeka@gmail.com>
2019-10-27 09:11:07 +1100

    removing

--- basics/src/resolvers/Subscription.ts
+++ basics/src/resolvers/Subscription.ts
@@ -1,19 +1,6 @@
 import { ICommentSub, IGqlCtx, IPost } from "../interfaces";
 
 export const Subscription = {
-    count: {
-        subscribe(parent: any, args: any, { pubsub }: IGqlCtx, info: any) {
-            let count = 0;
-
-            setInterval(() => {
-                count++;
-                pubsub.publish("count", { count });
-            }, 1000);
-
-            // async iterator takes a single argument called a channel name.
-            return pubsub.asyncIterator("count");
-        }
-    },
     comment: {
         subscribe(parent: any, args: ICommentSub, { db, pubsub }: IGqlCtx, info: any) {
             const post = db.posts.find((post) => post.id === args.postId && post.published);
