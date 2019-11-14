233966a465c43e0336c8f241735a34724eb50afb
akexinad <danixeka@gmail.com>
2019-11-15 07:22:36 +1100

    52. Mutations with Prisma Bindings

--- graphql-prisma/src/prisma.ts
+++ graphql-prisma/src/prisma.ts
@@ -1,10 +1,10 @@
 import { Prisma } from "prisma-binding";
-import { IComment, IUser } from "./interfaces";
+import { IComment, IPost, IUser } from "./interfaces";
 
 const prisma = new Prisma({
     /*
-    This file is initially configured via the .graphqlconfig file you need to write
-    that tells prisma where to store the typeDefs.
+    This file is initially configured via the .graphqlconfig file that you
+    need to write, telling prisma where to store the typeDefs.
     */
     typeDefs: "src/generated/prisma.graphql",
     // if your on windows you have to provide the IP address for the endpoint.
@@ -12,11 +12,45 @@ const prisma = new Prisma({
 });
 
 // @ts-ignore
-prisma.query.users(null, "{ id name email posts { id title body } }").then((data: IUser) => {
-    console.log(JSON.stringify(data, undefined, 2));
-});
+// prisma.query.users(null, "{ id name email posts { id title body } }").then((data: IUser) => {
+//     console.log(JSON.stringify(data, undefined, 2));
+// });
 
 // @ts-ignore
-prisma.query.comments(null, "{ id text author { id name } }").then((data: IComment) => {
+// prisma.query.comments(null, "{ id text author { id name } }").then((data: IComment) => {
+//     console.log(JSON.stringify(data, undefined, 2));
+// });
+
+prisma.mutation.createPost({
+    data: {
+        title: "ANOTHER graphql post from prisma",
+        body: "Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.",
+        published: true,
+        author: {
+            connect: {
+                id: "ck2wzinzs001i0796l2ahagca"
+            }
+        }
+    }
+    // @ts-ignore
+}, "{ id title body published author { id name email } }").then((data: IPost) => {
     console.log(JSON.stringify(data, undefined, 2));
+
+    // @ts-ignore
+    return prisma.query.users(null, "{ id name email posts { id title } }");
+}).then((data: IUser) => {
+    console.log(JSON.stringify(data, undefined, 2));
+
+    return prisma.mutation.updatePost({
+        where: {
+            id: "ck2z5fk0g000r0796wnrqaumm"
+        },
+        data: {
+            title: "UPDATED graphql post from prisma",
+            published: false
+        }
+    })
+    .then((data: IPost) => {
+        console.log(JSON.stringify(data, undefined, 2));
+    });
 });
