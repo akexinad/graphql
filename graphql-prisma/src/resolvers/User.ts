29c5b89fdb0fd8f14728c52e5f4e1d2bef9724f5
akexinad <danixeka@gmail.com>
2019-12-01 06:13:37 +1100

    fixed password resolver

--- graphql-prisma/src/resolvers/User.ts
+++ graphql-prisma/src/resolvers/User.ts
@@ -1,13 +1,14 @@
 import { GraphQLResolveInfo } from "graphql";
+import { Args } from "graphql-binding/dist/types";
 import { Context } from "graphql-yoga/dist/types";
-import { IBlogUser, IComment, IGraphQLContext, IPost } from "../interfaces";
+import { IBlogUser, IPost } from "../interfaces";
 import { getUserId } from "../utils/getUserId";
 
 export const User = {
 
     email: {
         fragment: "fragment userId on User { id }",
-        resolve(parent: IBlogUser, args: any, { request }: Context, info: GraphQLResolveInfo): IBlogUser["email"] {
+        resolve(parent: IBlogUser, args: Args, { request }: Context, info: GraphQLResolveInfo): IBlogUser["email"] {
 
             const userId = getUserId(request, false);
 
@@ -23,7 +24,7 @@ export const User = {
 
     posts: {
         fragment: "fragment userId on User { id }",
-        resolve(parent: IBlogUser, args: any, { prisma }: Context, info: GraphQLResolveInfo): IPost["published"] {
+        resolve(parent: IBlogUser, args: Args, { prisma }: Context, info: GraphQLResolveInfo): IPost["published"] {
 
             return prisma.query.posts({
                 where: {
@@ -37,19 +38,7 @@ export const User = {
         }
     },
 
-    password: {
-        fragment: "fragment userId on User { id }",
-        resolve(parent: IBlogUser, args: any, { request }: Context, info: GraphQLResolveInfo): IBlogUser["password"] {
-
-            const userId = getUserId(request, false);
-
-            // Now we can hide the emails of users that do not belong to the unauthenticated user when
-            // the authenticated user qeuries for users.
-            if (userId && userId === parent.id) {
-                return parent.password;
-            } else {
-                return null;
-            }
-        }
+    password(parent: IBlogUser, args: Args, { request }: Context, info: GraphQLResolveInfo): IBlogUser["password"] {
+        return "";
     }
 };
