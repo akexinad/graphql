ed8ccd437f76c1264322bcbfaec1f9bdeeb54968
akexinad <danixeka@gmail.com>
2019-12-03 22:35:04 +1100

    91. Node.js Production App Deployment: Part I

--- graphql-prisma/src/index.ts
+++ graphql-prisma/src/index.ts
@@ -24,6 +24,7 @@ const server: GraphQLServer = new GraphQLServer({
     fragmentReplacements
 });
 
-server.start(() => {
-    console.log("Server is running on http://localhost:4000");
+// heroku will inject an environment variable which exists on heroku, and there is only accessible from heroku.
+server.start({ port: process.env.PORT || 4000 }, () => {
+    console.log(`Server is running on ${ process.env.PORT ? process.env.PORT : "http://localhost:4000" }`);
 });
