40990219e43aec82d4da5a5fd6de76769114b8ee
akexinad <danixeka@gmail.com>
2019-12-04 20:52:47 +1100

    96. Installing and Exploring Jest

---
+++ graphql-prisma/tests/simple.test.js
@@ -0,0 +1,9 @@
+describe("this test", () => {
+  it("should be truthy", () => {
+    expect(true).toBeTruthy();
+  });
+
+  it("should render loading...", () => {
+      expect(false).toBeFalsy();
+  })
+});
