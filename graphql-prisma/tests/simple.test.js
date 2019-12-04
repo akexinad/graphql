e02a96709100fe751b20314f3ba5a9656eddf4b7
akexinad <danixeka@gmail.com>
2019-12-04 20:58:30 +1100

    added prettier file

--- graphql-prisma/tests/simple.test.js
+++ graphql-prisma/tests/simple.test.js
@@ -1,9 +1,9 @@
 describe("this test", () => {
-  it("should be truthy", () => {
-    expect(true).toBeTruthy();
-  });
+    it("should be truthy", () => {
+        expect(true).toBeTruthy();
+    });
 
-  it("should render loading...", () => {
-      expect(false).toBeFalsy();
-  })
+    it("should render loading...", () => {
+        expect(false).toBeFalsy();
+    });
 });
