47551235b48c19da5ebc46f2abbaaebd2b8f0974
akexinad <danixeka@gmail.com>
2019-12-05 12:23:11 +1100

    99. Apollo Client in the Browser: Part II

--- apollo-client/src/index.ts
+++ apollo-client/src/index.ts
@@ -4,6 +4,8 @@ const client = new ApolloBoost({
     uri: "http://localhost:4000"
 });
 
+console.log("hello world");
+
 const getUsers = gql`
     query {
         users {
@@ -13,10 +15,56 @@ const getUsers = gql`
     }
 `;
 
+const getPosts = gql`
+    query {
+        posts {
+            title
+            body
+            author {
+                name
+            }
+        }
+    }
+`;
+
+// client
+//     .query({
+//         query: getUsers
+//     })
+//     .then((response) => {
+//         let html = "";
+
+//         response.data.users.forEach((user: any) => {
+//             html += `
+//                 <div>
+//                     <h3>${ user.name}</h3>
+//                 </div>
+//             `;
+//         });
+
+//         // @ts-ignore
+//         document.getElementById("users").innerHTML = html;
+//     });
+
 client
     .query({
-        query: getUsers
+        query: getPosts,
     })
-    .then(response => {
+    .then((response) => {
         console.log(response);
+
+        let html = "";
+
+        response.data.posts.forEach((post: any) => {
+            html += `
+                <div>
+                    <h3>${ post.title }</h3>
+                    <p>${ post.body }</p>
+                    <p>BY: ${ post.author.name }</p>
+                </div>
+            `;
+        });
+
+        // @ts-ignore
+        document.getElementById("posts").innerHTML = html;
     });
