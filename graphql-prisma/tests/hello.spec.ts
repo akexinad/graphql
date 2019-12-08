068b7c0e38ca229ecb96d7952b23823af833a84c
akexinad <danixeka@gmail.com>
2019-12-08 20:17:51 +1100

    ignoring tests in the dist directory

---
+++ graphql-prisma/tests/hello.spec.ts
@@ -0,0 +1,13 @@
+describe("this test", () => {
+    it("should be truthy", () => {
+        expect(true).toBeTruthy();
+    });
+
+    it("should render loading... and no error", () => {
+        expect(false).toBeFalsy();
+    });
+
+    it("should render loading... and no error", () => {
+        expect(false).toBeFalsy();
+    });
+});
