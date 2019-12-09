891400b3f2c28e93377281a185350b49613b1678
akexinad <danixeka@gmail.com>
2019-12-09 20:28:25 +1100

    102. Seeding the Database with Test Data

---
+++ graphql-prisma/tests/simple.spec.ts
@@ -0,0 +1,5 @@
+describe("this test", () => {
+    it("should be truthy", () => {
+        expect(true).toBeTruthy();
+    });
+});
--- graphql-prisma/tests/user.spec.ts
+++ graphql-prisma/tests/user.spec.ts
@@ -4,11 +4,52 @@ import "cross-fetch/polyfill";
  * fetch API. See https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
  */
 
-import ApolloBoost, { gql, ExecutionResult } from "apollo-boost";
+import ApolloBoost, { gql } from "apollo-boost";
 import prisma from "../src/prisma";
+import bcrypt from "bcryptjs";
+import { IBlogUser } from "../src/interfaces";
 
 const client = new ApolloBoost({
     uri: "http://localhost:4000"
+});
+
+beforeEach(async () => {
+    await prisma.mutation.deleteManyPosts();
+    await prisma.mutation.deleteManyUsers();
+
+    const userWithPosts: IBlogUser = await prisma.mutation.createUser({
+        data: {
+            name: "pasolini",
+            email: "pasolini@ex.it",
+            password: bcrypt.hashSync("chicken1234")
+        }
+    });
+
+    await prisma.mutation.createPost({
+        data: {
+            title: "test title",
+            body: "test body",
+            published: true,
+            author: {
+                connect: {
+                    id: userWithPosts.id
+                }
+            }
+        }
+    });
+
+    await prisma.mutation.createPost({
+        data: {
+            title: "test draft title",
+            body: "test draft body",
+            published: false,
+            author: {
+                connect: {
+                    id: userWithPosts.id
+                }
+            }
+        }
+    });
 });
 
 describe("the user mutation", () => {
@@ -38,7 +79,6 @@ describe("the user mutation", () => {
 
         const userExists = await prisma.exists.User({
             id: response.data.createUser.user.id
-            // id: "ck3y6li1y00110707xbhqhs5m"
         });
 
         expect(userExists).toBe(true);
